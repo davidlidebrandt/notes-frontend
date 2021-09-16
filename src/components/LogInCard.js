0fb083f9ff264539c5e9eb21bea650449ad4e2c4
david <noemail>
2021-09-16 13:50:11 +0200

    Make loginform responsive

--- src/components/LogInCard.js
+++ src/components/LogInCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function LogInCard() {
     return (
-        <form className="col-span-12 blue-green-gradient rounded opacity-95 mt-36 mb-16 px-5 custom-b-shadow">
+        <form className="col-span-12 sm:col-span-10 sm:col-start-2 md:col-span-8 md:col-start-3 lg:col-span-6 lg:col-start-4 xl:col-span-4 xl:col-start-5 blue-green-gradient rounded opacity-95 mt-36 mb-16 px-5 custom-b-shadow">
             <h2 className="text-center text-3xl font-bold my-5">
                 <span className="">Log </span>
                 <span className="grey-text">In</span>
@@ -14,7 +14,7 @@ export default function LogInCard() {
             <button type="submit" className="d-blue-green-bg w-full grey-text font-bold text-2xl py-4 rounded border">Log In</button>
             <p className="text-center font-bold text-lg mt-12 grey-text">No account yet?</p>
             <a className="block m-auto text-center font-bold text-2xl py-3 w-2/3 rounded border mt-2">Sign Up</a>
-            <button className="block m-auto text-center font-bold text-2xl py-3 w-2/6 rounded border mt-12">Help</button>
+            <button className="block m-auto text-center yellow-bg opacity-80 border font-bold text-2xl py-3 w-2/6 rounded border mt-12">Help</button>
         </form>
     ) 
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,8 @@ export default function NavBar() {
     return (
         <nav className="fixed l-blue-green-bg  w-full py-4 grid grid-cols-2 custom-b-shadow">
                 <div className="grid items-center">
-                    <h1 className="inline text-3xl ml-3 font-extrabold">Notes <BiNotepad className="inline grey-text" size="30"/></h1>
-                    <p className="ml-3 text-lg font-extrabold italic ed-blue-green-text">All your notes in one place</p>
+                    <h1 className="inline text-3xl ml-3 font-extrabold ed-blue-green-text">Notes <BiNotepad className="inline" size="30"/></h1>
+                    <p className="ml-3 text-lg font-bold italic d-blue-green-text">All your notes in one place</p>
                 </div>
                 <div className="grid place-items-end">
                     <img className="inline h-16 mr-3" src={Ham}/>
