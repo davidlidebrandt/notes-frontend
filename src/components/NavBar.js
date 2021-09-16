0432361d5461e256725ee373dea5703479e73e0e
david <noemail>
2021-09-16 19:24:53 +0200

    Fix modal click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import { useState } from 'react'
 import Ham from "../images/ham.png"
+import {GrClose} from "react-icons/gr"
 
 
 export default function NavBar() {
     const [showModal, setShowModal] = useState("hidden")
+    const [showNav, setShowNav] = useState("")
+    
     return (
         <>
-          <div className={showModal + " absolute grey-bg top-0 left-0 w-full h-48 z-100"}>
-
+          <div className={showModal + " absolute m-blue-green-bg top-0 left-0 w-full h-48 z-100"}>
+            <GrClose onClick={()=> {setShowNav(""); setShowModal("hidden")}} className="m-auto" size="40"/>
          </div>
-        <nav className="fixed l-blue-green-bg  w-full py-4 grid grid-cols-2 custom-b-shadow" style={{zIndex: "-1"}}>
+        <nav className={showNav + "fixed l-blue-green-bg  w-full py-4 grid grid-cols-2 custom-b-shadow"}>
                 <div className="grid items-center">
                     <h1 className="inline text-3xl ml-3 font-extrabold ed-blue-green-text underline" style={{textDecorationColor: "#fec806"}}><span className="text-5xl">N</span>otes</h1>
                 </div>
                 <div className="grid place-items-end">
-                    <button onClick={()=> {setShowModal("block") }} className="border-none bg-none">
+                    <button onClick={()=> {setShowModal(""); setShowNav("hidden"); }} className="border-none bg-none">
                         <img alt="Hamburger icon" className="inline h-16 mr-3" src={Ham}/>
                     </button>
                 </div>
