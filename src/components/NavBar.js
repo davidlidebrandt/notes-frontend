c5a3f3122cd787cddfb51d22bf66327dd68fdc03
david <noemail>
2021-09-16 15:46:14 +0200

    Style login form

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ export default function NavBar() {
     return (
         <nav className="fixed l-blue-green-bg  w-full py-4 grid grid-cols-2 custom-b-shadow">
                 <div className="grid items-center">
-                    <h1 className="inline text-3xl ml-3 font-extrabold ed-blue-green-text underline"><span className="text-5xl">N</span>otes</h1>
+                    <h1 className="inline text-3xl ml-3 font-extrabold ed-blue-green-text underline" style={{textDecorationColor: "#fec806"}}><span className="text-5xl">N</span>otes</h1>
                 </div>
                 <div className="grid place-items-end">
                     <img className="inline h-16 mr-3" src={Ham}/>
