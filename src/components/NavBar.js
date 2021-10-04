ca827f8dbf73247940b944a5f3bca30b85facc23
David Lidebrandt <david.lidebrandt@gmail.com>
2021-10-04 15:20:06 +0200

    Add sidebar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import Ham from "../images/ham.png"
-import {GrClose} from "react-icons/gr"
+import {RiCloseCircleFill} from "react-icons/ri"
 
 
 export default function NavBar() {
@@ -10,8 +10,17 @@ export default function NavBar() {
     
     return (
         <>
-          <div className={showModal + " absolute m-blue-green-bg top-0 left-0 w-full h-48 z-100"}>
-            <GrClose onClick={()=> {setShowNav(""); setShowModal("hidden")}} className="m-auto" size="40"/>
+          <div className={showModal + " absolute m-blue-green-bg top-0 right-0 w-1/2 xl:w-1/4 min-h-screen z-100 py-3"}>
+          <h1 className="inline text-3xl ml-3 font-extrabold ed-blue-green-text underline" style={{textDecorationColor: "#fec806"}}><span className="text-5xl">N</span>otes</h1>
+          <div className="pb-20">
+          <a className="block text-center d-blue-green-bg mt-10  grey-bg ed-blue-green-text font-bold text-2xl py-4 rounded mx-5">Home</a>
+              <a className="block text-center d-blue-green-bg mt-10 grey-text font-bold text-2xl py-4 rounded mx-5">Log In</a>
+              <a className="block text-center font-bold mt-10 text-2xl py-4 rounded mt-2 grey-text yellow-bg mx-5">Sign Up</a>
+              <button className="absolute bottom-0 mb-10 block ed-blue m-auto-green-text mt-20 text-center center-absolute-x">
+          <RiCloseCircleFill onClick={()=> {setShowNav(""); setShowModal("hidden")}} className="ed-blue-green-text" size="80"/>
+            </button>  
+              </div>
+   
          </div>
         <nav className={showNav + "fixed l-blue-green-bg  w-full py-4 grid grid-cols-2 custom-b-shadow"}>
                 <div className="grid items-center">
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,7 +4,7 @@ import LogInCard from '../components/LogInCard'
 export default function Index() {
     return (
         <div className="grid grid-cols-12 min-h-screen px-10">
-            <div className="col-span-12 sm:col-span-10 sm:col-start-2 md:col-span-8 md:col-start-3 lg:col-span-6 lg:col-start-4 xl:col-span-4 xl:col-start-5 d-blue-green-bg m-auto rounded py-10 px-4 custom-b-shadow grey-text text-2xl mt-36" style={{zIndex: "-1000"}}><span className="l-blue-green-text" >A</span>ll your notes in one plac<span className="l-blue-green-text">e</span></div>
+            <div className="col-span-12 sm:col-span-10 sm:col-start-2 md:col-span-8 md:col-start-3   d-blue-green-bg m-auto rounded py-10 px-4 custom-b-shadow grey-text text-2xl mt-36" style={{zIndex: "-1000"}}><span className="l-blue-green-text" >A</span>ll your notes in one plac<span className="l-blue-green-text">e</span></div>
             <LogInCard/>
         </div>
     )
